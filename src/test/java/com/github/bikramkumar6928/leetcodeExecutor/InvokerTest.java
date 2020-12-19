b18a85f3012b989e5139db40c18c6ceeb8ec44db
BikramKumar6928 <bk6928@gmail.com>
2020-12-19 16:58:10 +0530

    Fixed compiler warnings

--- src/test/java/com/github/bikramkumar6928/leetcodeExecutor/InvokerTest.java
+++ src/test/java/com/github/bikramkumar6928/leetcodeExecutor/InvokerTest.java
@@ -2,9 +2,9 @@ package com.github.bikramkumar6928.leetcodeExecutor;
 
 import com.github.bikramkumar6928.leetcodeExecutor.sampleClasses.TestClass;
 import com.github.bikramkumar6928.leetcodeExecutor.sampleClasses.TestClassWithPrivateMethod;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class InvokerTest {
+public class InvokerTest {
     @Test
     public void testInvokerWithOnePublicMethod(){
         Invoker.invoke(TestClass.class, "1\n" +

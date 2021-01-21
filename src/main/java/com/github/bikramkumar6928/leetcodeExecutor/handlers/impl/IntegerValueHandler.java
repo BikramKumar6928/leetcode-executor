97e04df2f07658c476bc42b654bfb4cb16aec081
BikramKumar6928 <bk6928@gmail.com>
2021-01-21 11:17:14 +0530

    Working IntegerValueHandler

--- src/main/java/com/github/bikramkumar6928/leetcodeExecutor/handlers/impl/IntegerValueHandler.java
+++ src/main/java/com/github/bikramkumar6928/leetcodeExecutor/handlers/impl/IntegerValueHandler.java
@@ -2,11 +2,27 @@ package com.github.bikramkumar6928.leetcodeExecutor.handlers.impl;
 
 import com.github.bikramkumar6928.leetcodeExecutor.handlers.ValueHandler;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class IntegerValueHandler extends ValueHandler {
+
+    private static final Pattern pattern = Pattern.compile("\\d+\\s+");
 
     @Override
     public Object takeInput(String value) {
-        return 5;
+        String requiredValue = getRequiredValue(value);
+        return Integer.parseInt(requiredValue);
+    }
+
+    private String getRequiredValue(String value) {
+        Matcher matcher = pattern.matcher(value);
+        boolean isMatchFound = matcher.find();
+        if(!isMatchFound){
+            throw new RuntimeException("Match not for Integer in string " + value);
+        }
+        String foundMatch = matcher.group();
+        return foundMatch.trim();
     }
 
     @Override

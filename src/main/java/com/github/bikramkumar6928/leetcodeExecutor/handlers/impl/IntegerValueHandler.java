c3aea4ddd71f2a5c096619d45c863ceb1968e7cb
BikramKumar6928 <bk6928@gmail.com>
2021-01-25 01:12:35 +0530

    Moved to newer representation of ValueHandlers.

--- src/main/java/com/github/bikramkumar6928/leetcodeExecutor/handlers/impl/IntegerValueHandler.java
+++ src/main/java/com/github/bikramkumar6928/leetcodeExecutor/handlers/impl/IntegerValueHandler.java
@@ -1,28 +1,22 @@
 package com.github.bikramkumar6928.leetcodeExecutor.handlers.impl;
 
+import com.github.bikramkumar6928.leetcodeExecutor.beans.UpdatedInputAndParameter;
 import com.github.bikramkumar6928.leetcodeExecutor.handlers.ValueHandler;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class IntegerValueHandler extends ValueHandler {
 
-    private static final Pattern pattern = Pattern.compile("\\d+\\s+");
+    private static final Pattern pattern = Pattern.compile("\\d+\\s*");
 
     @Override
-    public Object takeInput(String value) {
-        String requiredValue = getRequiredValue(value);
-        return Integer.parseInt(requiredValue);
+    public Pattern getPattern() {
+        return pattern;
     }
 
-    private String getRequiredValue(String value) {
-        Matcher matcher = pattern.matcher(value);
-        boolean isMatchFound = matcher.find();
-        if(!isMatchFound){
-            throw new RuntimeException("Match not for Integer in string " + value);
-        }
-        String foundMatch = matcher.group();
-        return foundMatch.trim();
+    @Override
+    protected Object processMatch(String foundMatch) {
+        return Integer.parseInt(foundMatch);
     }
 
     @Override

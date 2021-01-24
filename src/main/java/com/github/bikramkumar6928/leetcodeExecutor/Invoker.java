ec730d693d38f27fa86e5b45a3bd6b96384a1357
BikramKumar6928 <bk6928@gmail.com>
2021-01-25 01:13:43 +0530

    Moved from forEach to loops to allow updating of input.

--- src/main/java/com/github/bikramkumar6928/leetcodeExecutor/Invoker.java
+++ src/main/java/com/github/bikramkumar6928/leetcodeExecutor/Invoker.java
@@ -1,5 +1,6 @@
 package com.github.bikramkumar6928.leetcodeExecutor;
 
+import com.github.bikramkumar6928.leetcodeExecutor.beans.UpdatedInputAndParameter;
 import com.github.bikramkumar6928.leetcodeExecutor.handlers.ValueHandler;
 import com.github.bikramkumar6928.leetcodeExecutor.handlers.ValueHandlerUtils;
 import com.google.common.collect.ArrayListMultimap;
@@ -17,24 +18,26 @@ import java.util.stream.Collectors;
 @Slf4j
 public class Invoker {
     public static void invoke(Class<?> clazz, String input){
-        List<Method> methods = Arrays
+        List<Method> publicMethods = Arrays
                 .stream(clazz.getDeclaredMethods())
                 .filter(Invoker::isPublicMethod)
                 .collect(Collectors.toList());
         ListMultimap<Method,Object> mapListObject = ArrayListMultimap.create();
-        methods.forEach(method ->{
-                    List<Class<?>> parameterClasses = Arrays.asList(method.getParameterTypes());
-                    parameterClasses.forEach(parameterClass ->{
-                        ValueHandler valueHandler = ValueHandlerUtils.getObject(parameterClass);
-                        mapListObject.put(method, valueHandler.takeInput(input));
-                    });
-                });
+        for (Method publicMethod : publicMethods) {
+            Class<?>[] parameterClasses = publicMethod.getParameterTypes();
+            for (Class<?> parameterClass : parameterClasses) {
+                ValueHandler valueHandler = ValueHandlerUtils.getObject(parameterClass);
+                UpdatedInputAndParameter updatedInputAndParameter = valueHandler.takeInput(input);
+                input = updatedInputAndParameter.getUpdatedInput();
+                mapListObject.put(publicMethod, updatedInputAndParameter.getParameter());
+            }
+        }
         log.trace("The map is {}", mapListObject);
         Object classObject = getClassObject(clazz);
-        methods.forEach(method -> {
-                    Object[] parameters = getParametersForMethod(method, mapListObject);
-                    callMethod(method, classObject, parameters);
-                });
+        for (Method method : publicMethods) {
+            Object[] parameters = getParametersForMethod(method, mapListObject);
+            callMethod(method, classObject, parameters);
+        }
 
     }
 

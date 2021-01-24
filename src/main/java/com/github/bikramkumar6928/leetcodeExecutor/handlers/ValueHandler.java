f8c81d0ddd55ca3e830debca713c5057f0f5c7d6
BikramKumar6928 <bk6928@gmail.com>
2021-01-25 01:08:37 +0530

    Removed hard bound pattern and added added function for support of type specific pattern

--- src/main/java/com/github/bikramkumar6928/leetcodeExecutor/handlers/ValueHandler.java
+++ src/main/java/com/github/bikramkumar6928/leetcodeExecutor/handlers/ValueHandler.java
@@ -2,7 +2,7 @@ package com.github.bikramkumar6928.leetcodeExecutor.handlers;
 
 public abstract class ValueHandler {
 
-    public static final String pattern = " \\n";
+    public abstract Pattern getPattern();
 
     public abstract Object takeInput(String value);
 

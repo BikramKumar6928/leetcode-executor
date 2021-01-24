951d6d59b249d7862b73772e6cf2a07a2350c349
BikramKumar6928 <bk6928@gmail.com>
2021-01-25 01:09:49 +0530

    Added generic input taking implementation to class

--- src/main/java/com/github/bikramkumar6928/leetcodeExecutor/handlers/ValueHandler.java
+++ src/main/java/com/github/bikramkumar6928/leetcodeExecutor/handlers/ValueHandler.java
@@ -1,10 +1,30 @@
 package com.github.bikramkumar6928.leetcodeExecutor.handlers;
 
+import com.github.bikramkumar6928.leetcodeExecutor.beans.UpdatedInputAndParameter;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public abstract class ValueHandler {
 
     public abstract Pattern getPattern();
 
-    public abstract Object takeInput(String value);
+    public UpdatedInputAndParameter takeInput(String value) {
+        Matcher matcher = getPattern().matcher(value);
+        boolean isMatchFound = matcher.find();
+        if(!isMatchFound){
+            throw new RuntimeException("Match not for Integer in string " + value);
+        }
+        String foundMatch = matcher.group().trim();
+        Object processedMatch = processMatch(foundMatch);
+        return UpdatedInputAndParameter
+                .builder()
+                .parameter(processedMatch)
+                .updatedInput(value.substring(matcher.end()))
+                .build();
+    }
+
+    protected abstract Object processMatch(String foundMatch);
 
     public abstract Class<?> getClazz();
 }

1f8ed044e0856eef2ae2381c170459ffc93d78f7
BikramKumar6928 <bk6928@gmail.com>
2021-03-30 21:12:27 +0530

    Added int value handler

---
+++ src/main/java/com/github/bikramkumar6928/leetcodeExecutor/handlers/impl/IntValueHandler.java
@@ -0,0 +1,8 @@
+package com.github.bikramkumar6928.leetcodeExecutor.handlers.impl;
+
+public class IntValueHandler extends IntegerValueHandler{
+    @Override
+    protected Class<?> getClazz() {
+        return int.class;
+    }
+}
